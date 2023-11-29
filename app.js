247bb1429ef1220f9d4da822d35fdb8b4eddcbe2
LeVDuy <levanduyhd98@gmail.com>
2023-11-29 15:52:17 +0100

    Update to main Duy

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import UserModel from './Model/UserModel.js'
 import UserView from './View/UserView.js'
 import UserController from './Controller/UserController.js'
 
-const MonUserController = new UserController(new UserModel(), new UserView())
-console.log(MonUserController)
-MonUserController.addUser('Quentin')
-console.log(MonUserController.getUsers())
+const userView = new UserView();
+const userModel = new UserModel();
+const userController = new UserController(userModel, userView);
